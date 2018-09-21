32669ca8f45a90d2d80a808f4565071ec0fd14d5
Mark Yen <mark.yen@suse.com>
2018-09-20 17:22:25 -0700

    Hack to make this use mongo

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import json
+import pymongo
 from bottle import route, run, template
+from pprint import pprint
 
 print("Running using: %s" % sys.executable)
 
@@ -13,11 +16,24 @@ html_template = r'''
     <li><b>{{name}}</b>: <tt>{{value}}</tt></li>
   % end
 </ul>
+Extra stuff: <pre>{{extra}}</pre>
 </body>
 '''
 
 @route('/')
 def index():
-    return template(html_template, envlist=os.environ)
+    result = None
+    try:
+      services = json.loads(os.environ['VCAP_SERVICES'])
+      mongo_uri = services["mongodb"][0]["credentials"]["uri"]
+      client = pymongo.MongoClient(mongo_uri)
+      db = client.mydb
+      business = {'name': 'SUSE', 'hq': 'Nürnberg'}
+      result = db.reviews.insert_one(business)
+    except:
+      result = sys.exc_info()
+    if isinstance(result, pymongo.results.InsertOneResult):
+      result = (result.inserted_id, result.acknowledged)
+    return template(html_template, envlist=os.environ, extra=result)
 
 run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)))
